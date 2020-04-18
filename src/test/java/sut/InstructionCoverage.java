c82ef1a1f5bab331d54bad7728c1c50d457421d9
Vasco Ferreira <vascobferreira98@gmail.com>
2020-04-18 16:52:00 +0100

    Added one case to keysWithPrefix but doesnt seem to make a difference

--- src/test/java/sut/InstructionCoverage.java
+++ src/test/java/sut/InstructionCoverage.java
@@ -51,6 +51,7 @@ public class InstructionCoverage {
 	@Test
 	public void testGetWithEmptyKey() {
 		TST<Integer> st = new TST<>();
+		st.put("string", 0);
 		assertThrows(IllegalArgumentException.class, () -> {
 			st.get("");
 		});
