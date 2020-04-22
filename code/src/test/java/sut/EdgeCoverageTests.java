46a389760bba51a5239e9625a1d5e10d6ea30f09
Vasco Ferreira <vascobferreira98@gmail.com>
2020-04-22 16:41:44 +0100

    added comments of the test paths and the TestRequirements

--- code/src/test/java/sut/EdgeCoverageTests.java
+++ code/src/test/java/sut/EdgeCoverageTests.java
@@ -6,7 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class EdgeCoverageTests {
+	
+	/*
+	 * Test Requirements:
+	 * TR(EC) = {[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[1,2],[1,3],[3,4],
+	 *  [3,5],[5,6],[6,7],[6,13],[7,8],[7,9],[7,10],[8,6],[9,6],[10,12],[10,11][11,12],[12,6]}
+	 */
 
+	/*
+	 * Test Path:
+	 * [1,2]
+	 */
 	@Test
 	public void testLongestPrefixOfWithNullQuery() {
 		TST<Integer> st = new TST<>();
@@ -15,6 +25,10 @@ public class EdgeCoverageTests {
 		});
 	}
 	
+	/*
+	 * Test Path:
+	 * [1,3,4]
+	 */
 	@Test
 	public void testLongestPrefixOfWithEmptyQuery() {
 		TST<Integer> st = new TST<>();
@@ -22,6 +36,10 @@ public class EdgeCoverageTests {
 		assertEquals(null, result);
 	}
 	
+	/*
+	 * Test Path:
+	 * [1,3,5,6,7,8,6,7,9,6,7,10,11,12,6,13]
+	 */
 	@Test
 	public void testLongestPrefixOfWithValidQuery1() {
 		TST<Integer> st = new TST<>();
@@ -33,6 +51,10 @@ public class EdgeCoverageTests {
 		assertEquals(expected, result);
 	}
 	
+	/*
+	 * Test Path:
+	 * [1,3,5,6,7,10,12,6,13]
+	 */
 	@Test
 	public void testLongestPrefixOfWithValidQuery2() {
 		TST<Integer> st = new TST<>();
