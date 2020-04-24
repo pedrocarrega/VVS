49ab965167c517a079e373084abe099891cc4393
Pedro Carrega <pedro.carrega@outlook.com>
2020-04-24 12:30:56 +0100

    more tests

--- code/src/test/java/sut/PrimePathCoverage.java
+++ code/src/test/java/sut/PrimePathCoverage.java
@@ -1,5 +1,6 @@
 package sut;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,14 +16,46 @@ public class PrimePathCoverage {
 			st.longestPrefixOf(null);
 		});
 	}
-	
+
 	@Test
 	public void testLongestPrefixOfWithEmptyQuery() {
+		//[1, 3, 4]
 		TST<Integer> st = new TST<>();
 		String result = st.longestPrefixOf("");
 		assertNull(result);
 	}
+
+	@Test
+	public void testLongestPrefixOfValid1() {
+		//[1,3,5,6,7,8]
+		//[7,8,6,7]
+		//[7,9,6,13]
+		//[6,7,9,6]
+		//[6,7,8,6]
+		//[8,6,7,9]
+		TST<Integer> st = new TST<>();
+		st.put("c", 0);
+		st.put("a", 0);
+		String result = st.longestPrefixOf("b");
+		
+		assertEquals("", result);
+	}
 	
-	
-	
+	@Test
+	public void testLongestPrefixOfValid2() {
+		//[1,3,5,6,7,9]
+		//[7,9,6,7]
+		//[7,9,6,13]
+		//[6,7,9,6]
+		//[9,6,7,9]
+		TST<Integer> st = new TST<>();
+		st.put("d", 0);
+		st.put("c", 0);
+		String result = st.longestPrefixOf("b");
+		
+		assertEquals("", result);
+	}
+
+
+
 }
