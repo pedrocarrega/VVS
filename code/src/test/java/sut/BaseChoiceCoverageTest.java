46ccaea65f4f836c6981265aace319a677264f2a
Vasco Ferreira <vascobferreira98@gmail.com>
2020-05-06 23:12:56 +0100

    Correction BCC

--- code/src/test/java/sut/BaseChoiceCoverageTest.java
+++ code/src/test/java/sut/BaseChoiceCoverageTest.java
@@ -8,69 +8,120 @@ public class BaseChoiceCoverageTest {
 	/**
 	 * Input State Partitioning
 	 * 
-	 * Vamos assumir que todos as características são do tipo Bool
-	 * Trie is empty => Empty or !Empty => True || False
-	 * Trie contains elem => Contains or !Contains => True || False
-	 * Trie already includes some new key prefix ==> Includes or !Includes ==> true || false
-	 * Sendo assim, podemos pegar num caso base [False,False, False] -> fazível
-	 * [True, False, False] -> fazível
-	 * [True, True, False] -> Não é fazível, em casos onde a primeira condição se verifica verdade podermos testar
-	 * qualquer uma das seguintes como tu, uma trie sem elementos nunca pode conter um elemento que estamos a querer pesquisar/dar overwrite
-	 * [True, True, True] -> Para além do descrito acima, uma trie vazia não pode ter um key prefix 
-	 *	TODO restantes y/n? 
+	 * Caracteristics:
+	 * 	1- Trie is empty
+	 * 	2- Trie already includes the new key
+	 *  3- Trie already includes some new key prefix
+	 *  
+	 *  With this characteristics we can verify that all of them have equal blocks, that are [true,false]
+	 *  So starting with the following base choice:
+	 *  [true,true,true] 		TR1
+	 *  [false,true,true]		TR2
+	 *  [false,false,true]		TR3
+	 *  [false,false,false]		TR4
+	 *  
+	 *  With this 4 Test Requirements we can cover all possible values of each characteristic
+	 *  One important note is that TR1 is infeasible since we can not have an empty Trie were the Trie already included the new key
+	 *  All the other Test Requirements will be feasible and be verified, TR1 is never going to be verified
+	 */
+	
+	/*
+	 * -Covers TR1
+	 * Infeasible Test Requirement
 	 */
 	@Test
-	public void testPutAlreadyExistingKey() {
+	public void testPutWithEmptyTrie() {
+		TST<Integer> st = new TST<>();
+		String input = "a";
+		
+		//This covers the property 1- Trie is empty
+		boolean is_empty = st.size() == 0;
+		assertTrue(is_empty);
+		
+		//This covers the property 2- Trie already includes the new key
+		boolean contains_key = st.contains(input);
+		assertTrue(contains_key);
+		
+		//This covers the property 3- Trie already includes some new key prefix
+		String prefix_found = st.longestPrefixOf(input);
+		boolean contains_prefix = !prefix_found.equals("");
+		assertTrue(contains_prefix);	
+				
+		st.put(input,0);
+	}
+	
+	/*
+	 * -Covers TR2
+	 */
+	@Test
+	public void testPutNonEmptyTrieWithKeyAndPrefix() {
 		TST<Integer> st = new TST<>();
 		st.put("a", 0);
-		st.put("a", 1);
-		String expected = "1";
-		String result = st.get("a").toString();
-		assertEquals(expected, result);
+		st.put("ab", 1);
+		String input = "a";
+		
+		//This covers the property 1- Trie is empty
+		boolean is_empty = st.size() == 0;
+		assertFalse(is_empty);
+		
+		//This covers the property 2- Trie already includes the new key
+		boolean contains_key = st.contains(input);
+		assertTrue(contains_key);
+		
+		//This covers the property 3- Trie already includes some new key prefix
+		String prefix_found = st.longestPrefixOf(input);
+		boolean contains_prefix = !prefix_found.equals("");
+		assertTrue(contains_prefix);
+		
+		st.put(input, 0);
 	}
 	
+	/*
+	 * -Covers TR3
+	 */
 	@Test
-	public void testPutAlreadyExistingKey2() {
-		TST<Integer> st = new TST<>();//empty trie
-		st.put("a", 0);
-		st.put("b", 1);
-		String expected = "0";
-		String result = st.get("a").toString();
-		assertEquals(expected, result);
-	}
-	
-	@Test
-	public void testPutAlreadyExistingKey3() {
-		TST<Integer> st = new TST<>();//empty trie
-		st.put("a", 0);
-		st.put("a", 2);
-		st.put("a", 0);
-		st.put("b", 2);
-		st.put("a", 4);
-		st.put("a", 0);
-		st.put("b", 3);
-		st.put("a", 7);
-		st.put("b", 1);
-		String expected = "7";
-		String result = st.get("a").toString();
-		assertEquals(expected, result);
-	}
-	
-	
-	@Test
-	public void testKeyPrefixWithEmptyTST() {
+	public void testPutNonEmptyTrieWithPrefix() {
 		TST<Integer> st = new TST<>();
-		String result = st.longestPrefixOf("abcd");
-		String expected = "";
-		assertEquals(expected,result);
+		String input = "ab";
+		
+		st.put("a", 0);
+		
+		//This covers the property 1- Trie is empty
+		boolean is_empty = st.size() == 0;
+		assertFalse(is_empty);
+		
+		//This covers the property 2- Trie already includes the new key
+		boolean contains_key = st.contains(input);
+		assertFalse(contains_key);
+		
+		//This covers the property 3- Trie already includes some new key prefix
+		String prefix_found = st.longestPrefixOf(input);
+		boolean contains_prefix = !prefix_found.equals("");
+		assertTrue(contains_prefix);
 	}
 	
+	/*
+	 * -Covers TR4
+	 */
 	@Test
-	public void testPutOnEmptyTrie() {
+	public void testPutNonEmptyTrieWithoutKeyOrPrefix() {
 		TST<Integer> st = new TST<>();
-		int expected = 0;
-		int size1 = st.size();
-		assertEquals(expected, size1);
+		String input = "b";
+		st.put("a", 0);
+		
+		//This covers the property 1- Trie is empty
+		boolean is_empty = st.size() == 0;
+		assertFalse(is_empty);
+		
+		//This covers the property 2- Trie already includes the new key
+		boolean contains_key = st.contains(input);
+		assertFalse(contains_key);
+		
+		//This covers the property 3- Trie already includes some new key prefix
+		String prefix_found = st.longestPrefixOf(input);
+		boolean contains_prefix = !prefix_found.equals("");
+		assertFalse(contains_prefix);
+				
+		st.put(input, 1);
 	}
-
 }
